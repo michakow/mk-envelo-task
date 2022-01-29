d96d5f5f70ca06c360adcc2be991ec9b22fb5efe
Michal Kowalczyk <mickowalczyk6@gmail.com>
2022-01-29 02:50:47 +0100

    validate inputs

--- js/showInputs.js
+++ js/showInputs.js
@@ -18,15 +18,62 @@ export const showInputs = (main, button) => {
   inputPhone.setAttribute('type', 'tel');
   inputCode.setAttribute('type', 'text');
 
+  const validText = document.createElement('p');
+  validText.className = 'valid-text';
+
   labelPhone.appendChild(inputPhone);
   labelCode.appendChild(inputCode);
   inputs.appendChild(labelPhone);
   inputs.appendChild(labelCode);
+  inputs.appendChild(validText);
   main.insertBefore(inputs, button);
 
+  const validate = () => {
+    if(inputPhone.value.trim() && inputCode.value.trim()){
+      if(inputPhone.value.length === 9){
+        const phoneValid = /^[0-9]+$/;
+        const valid = inputPhone.value.match(phoneValid);
+        if(!!inputPhone.value.match(phoneValid) === false){
+          validText.textContent = 'Nieprawidłowy numer telefonu (format: 555777333)';
+          validText.classList.add('active');
+          return false;
+        }else{
+          if(inputCode.value.length === 4){
+            const codeValid = /^[0-9]+$/;
+            const valid = inputCode.value.match(codeValid);
+            if(!!inputCode.value.match(codeValid) === false){
+              validText.textContent = 'Nieprawidłowy kod odbioru (format: 4503)';
+              validText.classList.add('active');
+              return false;
+            }else{
+              validText.textContent = '';
+              validText.classList.remove('active');
+              return true;
+            }
+          }else{
+            validText.textContent = 'Nieprawidłowy kod odbioru (format: 4503)';
+            validText.classList.add('active');
+            return false;
+          }
+        } 
+      }else{
+        validText.textContent = 'Nieprawidłowy numer telefonu (format: 555777333)';
+        validText.classList.add('active');
+        return false;
+      }
+    }else{
+      validText.textContent = 'Nie wszystkie pola zostały wypełnione';
+      validText.classList.add('active');
+      return false;
+    } 
+  };
+
   const onShowModal = () => {
-    showModal(main, button, inputs);
-    button.removeEventListener('click', onShowModal);
+    const valid = validate();
+    if(valid){
+      showModal(main, button, inputs);
+      button.removeEventListener('click', onShowModal);
+    }
   };
 
   button.addEventListener('click', onShowModal);
